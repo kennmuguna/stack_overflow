aea6dcb5459e836a772ca595852b54633bff68bf
kennmuguna <kennmuguna@gmail.com>
2017-03-22 15:42:42 +0300

    password not getting picked up

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,10 +1,27 @@
 class AnswersController < ApplicationController
   def new
+    @question = Question.find(params[:question_id])
+    @answer = @question.answers.new
+  end
+
+  def create
+    @question = Question.find(params[:question_id])
+    @answer = @question.answers.new(answer_params)
+    if @answer.save
+      redirect_to question_path(@answer.question)
+    else  
+      render :new
+    end
   end
 
   def show
   end
 
   def edit
+  end
+
+  private
+  def answer_params
+    params.require(:answer).permit(:name, :response)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,11 @@
-class User < ApplicationRecord
+class User < ActiveRecord::Base
+    has_secure_password
+
+private
+def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation, users_attributes: [:id, :email, :password, :password_configuration])
+end
+
+    
+    validates_uniqueness_of :email
 end
